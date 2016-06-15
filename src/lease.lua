90660fbdcc42b3c4222c35dab7a4c72913777102
GeorgyKirichenko <kirichenkoga@gmail.com>
2016-06-15 12:47:36 +0300

    Update quotas, some refactoring, add commects

--- src/lease.lua
+++ src/lease.lua
@@ -1,140 +1,125 @@
 #!/usr/local/bin/tarantool
 
+-- Distributed rps quota library using gossip protocol
+-- Main params - connection pool with connections to gossip members,
+-- count of neighbours to distrubute updates on each gossip round
+-- Resource identifies by user, resource domain and operation
+-- (e.g. user1, subscriptions, write)
+-- Each second sliced to some count of intervals, for each of them we 
+-- count amount of used resource. For time we use scaled time abstraction, so
+-- scaled_time = [time * count_of_slices_in_second].
+
 local fiber = require('fiber')
 local math = require('math')
-local pickle = require('pickle')
-local digest = require('digest')
 
--- count of slices in one second
-local slices = 10
+-- cache lua functions
+local mmin = math.min
+local mmax = math.max
+local mmod = math.mod
+local mrandom = math.random
+local mfloor = math.floor
+
+
+-- Quotas constants
+-- count of SLICES in one second
+local SLICES = 10
 -- max count of lease anounces grouped in one rpc call
-local task_pack_size = 10
+local PACK_SIZE = 10
 
--- cluster connection pool
-local conn_pool
--- count of cluster members to send lease announces on each round
-local neighbor
+-- Quotas params
 -- max amount for granting
 local max_lease
 
--- summary list of granted leases
--- maps resource key to resource usage
--- resource usage consists of array [1 .. slices] of numbers
--- and last update time
-local leases = {}
--- local reminders of recieved leases
-local reminders = {}
--- announces about used leases for each cluster member
-local announces = {}
+-- Gossip params
+-- cluster connection pool
+local conn_pool
+-- count of cluster members to send lease announces on each round
+local neighbour_count
+-- local server id
+local server_id
+
+-- Quotas cluster state
+local grants = {}
+
+-- Local grant remainders
+local remainders = {}
 
 local gossip_queue
 local gossip_fiber
 
--- max portion of resource in one lease
-local function get_max_lease(key)
-    return max_lease
-end
-
 -- build resource key from user, domain and operation
-local function get_key(user, domain, op)
+local function build_resource_key(user, domain, op)
     return user .. '::' .. domain .. '::' .. op
 end
 
--- get use for resource
-local function get_usage(key, stime)
-    local usage = leases[key]
-    -- no info about leases, build it
-    if usage == nil then
-        usage = {history = {}, stime = stime}
-        for i = 1, slices do
-            usage.history[i] = 0
-        end
-        leases[key] = usage
-    end
-    history = usage.history
-    -- Zeroing all outdated slices
-    for i = usage.stime + 1,
-        math.min(stime, usage.stime + slices) do
-        history[1 + math.mod(i, slices)] = 0
-    end
-
-    if usage.stime < stime then
-        usage.stime = stime
-    end
-    -- Outdated request
-    if usage.stime - slices > stime then
-        return nil
-    end
-    return usage
+-- build key for resource and server
+local function build_server_resource_key(server, resource_key)
+    return server .. '::' .. resource_key
 end
 
--- get usage for server and resource
-local function get_announce(srv, key, stime)
-    -- if we have no info about usage resource on server - create it
-    if announces[srv] == nil then
-        announces[srv] = {[key] = {stime = stime, history = {}}}
-        for i = 1, slices do
-            announces[srv][key].history[i] = 0
+-- check if scaled_time is outdated, renew grant info (clear outdated history)
+local function renew_grant(grant, scaled_time)
+    if grant.update_time and scaled_time <= grant.update_time - SLICES then
+        return false
+    end
+    if grant.update_time and scaled_time <= grant.update_time then
+        return true
+    end
+    if grant.update_time == nil then
+        grant.update_time = scaled_time
+        for i = 0, SLICES - 1 do
+            grant[i] = 0
         end
+        return true
     end
-    if announces[srv][key] == nil then
-        announces[srv][key] = {stime = stime, history = {}}
-        for i = 1, slices do
-            announces[srv][key].history[i] = 0
-        end
+    
+    for i = 1, mmin(SLICES, scaled_time - grant.update_time) do
+        grant[mmod(grant.update_time + i, SLICES)] = 0
     end
-    announce = announces[srv][key]
-    -- clear outdated info
-    for i = announce.stime + 1,
-        math.min(stime, announce.stime + slices) do
-        announce.history[1 + math.mod(i, slices)] = 0
-    end
-    if announce.stime < stime then
-        announce.stime = stime
-    end
-    -- check for outdated request
-    if announce.stime - slices > stime then
-        return nil
-    end
-    return announce
+    grant.update_time = scaled_time
+    return true
 end
 
--- send/forward lease info pack to neigbours
-local function send_lease(servers, task_pack)
-    local g_size = #servers
-    if conn_pool.self_server ~= nil then
-        g_size = g_size - 1
+-- return grant info by key (server or cluster and resource)
+local function get_grant(key)
+    local grant = grants[key]
+    if grant == nil then
+        grant = {}
+        grants[key] = grant
     end
-    -- count of neigbours to send
-    local left = math.min(neighbor, g_size)
-    local processed = 0
-    for i = 1, #servers do
-        -- don't send to self
-        if servers[i] ~= conn_pool.self_server then
-            if math.mod(pickle.unpack('i', digest.urandom(4)),
-                (g_size - processed)) < left then
-                servers[i].conn:call('pop_lease', task_pack)
-                left = left - 1
-            end
-            processed = processed + 1
-        end
-        if left == 0 then
-            break
-        end
-    end
+    return grant
 end
 
-local function lease_task(srv, key, stime, used)
-    return {
-        mode = 'pop',
-        srv = srv,
-        key = key,
-        stime = stime,
-        used = used}
+-- get resource grants for cluster
+local function get_cluster_grant(resource_key)
+    return get_grant(resource_key)
 end
 
--- reads info about about leases, apply corresponding changes to usage info
--- and forward datas to next neigbours
+-- get resource grant for server
+local function get_server_grant(server, resource_key)
+    return get_grant(build_server_resource_key(server, resource_key))
+end
+
+-- select random count items from servers excluding local
+local function select_neighbours(servers, count)
+    local neighbours = {}
+    for _, server in pairs(servers) do
+        if server ~= conn_pool.self_server then
+            neighbours[#neighbours + 1] = server
+        end
+    end
+    count = mmin(#neighbours, count)
+    local selected = {}
+    for i = 1, count do
+        local index = mrandom(#neighbours - i)
+        selected[i] = neighbours[index]
+        neighbours[index] = neighbours[#neighbours]
+        neighbours[#neighbours] = selected[i]
+    end
+    return selected
+end
+
+-- forward grant info to next neigbours
 local function gossip_fiber(queue)
     while true do
         local task = queue:get()
@@ -142,110 +127,129 @@ local function gossip_fiber(queue)
         local task_pack = {}
         local task_count = 0
         repeat
-            local index = 1 + math.mod(task.stime, slices)
-            local announce = get_announce(task.srv, task.key, task.stime)
-	    -- we already have info about resource usage on server
-            if announce ~= nil then
-                if task.mode == 'push' then
-		    -- lease info from local server, just update usage info
-                    announce.history[index] = announce.history[index] + task.grant
+            local index = mmod(task.scaled_time, SLICES)
+            local server_grant = get_server_grant(task.server, task.key)
+	    -- check if grant info is not outdated
+            if renew_grant(server_grant, task.scaled_time)
+                and (server_grant[index] < task.grant or task.server == server_id) then
+                local server_key = build_server_resource_key(task.server, task.key)
+                if task_pack[server_key] then
+		    -- update exisiting task in pack
+                    task_pack[server_key].grant = server_grant[index]
                 else
-		    -- lease info from neigbour
-                    local usage = get_usage(task.key, task.stime)
-                    if announce.history[index] < task.used and usage ~= nil then
-		        -- update usage info
-                        usage.history[index] = usage.history[index] +
-                            task.used - announce.history[index]
-                        announce.history[index] = task.used
-                    else
-		        -- we already know about greather resource usage or there is outdated request
-                        task = nil
-                    end
-                end
-                if task ~= nil then
-		    -- and info to send to gossip neighbours
-                    task_pack[task.srv .. '::' .. task.key] =
-                        lease_task(task.srv, task.key, task.stime, announce.history[index])
+                    task.grant = server_grant[index]
+                    task_pack[server_key] = task
                     task_count = task_count + 1
                 end
             end
-            if task_count >= task_pack_size then
+            if task_count >= PACK_SIZE then
                 break
             end 
             task = queue:get(0)
         until task == nil
         if task_count > 0 then
-	    -- send task pack
-            send_lease(servers, task_pack)
+            -- send task pack
+            for _, neighbour in pairs(select_neighbours(servers, neighbour_count)) do
+                neighbour.conn:call('pop_lease', task_pack)
+            end
         end
     end
 end
 
--- accept lease info pack and add it to gossip queue
+-- apply grant info update to cluster state
+local function update_grant(task)
+    local index = mmod(task.scaled_time, SLICES)
+    local server_grant = get_server_grant(task.server, task.key)
+    if not renew_grant(server_grant, task.scaled_time) then
+        return false
+    end
+    local delta = task.grant - server_grant[index]
+    if delta <= 0 then
+        return false
+    end
+    server_grant[index] = server_grant[index] + delta
+    local cluster_grant = get_cluster_grant(task.key)
+    if not renew_grant(cluster_grant, task.scaled_time) then
+        return false
+    end
+    cluster_grant[index] = cluster_grant[index] + delta
+    return true
+end
+
+-- accept grant info pack, apply it to cluster state and forward
 function pop_lease(task_pack)
     for i, task in pairs(task_pack) do
-        if task.srv ~= box.info.server.uuid then
+        if task.server ~= server_id and update_grant(task) then
             gossip_queue:put(task)
         end
     end
 end
 
--- request resource lease
+-- request resource grant
 local function request_lease(key, quota, time)
     -- get "scaled" time
-    local stime = math.floor(time * slices)
-    -- evaluate resource usage
-    local usage = get_usage(key, stime)
-    local history = usage.history
-    local quota_usage = 0
-    for i = stime - slices + 1, usage.stime do
-        quota_usage = quota_usage + history[1 + math.mod(i, slices)]
+    local scaled_time = mfloor(time * SLICES)
+    -- calculate resource usage for last 1 second
+    local cluster_grant = get_cluster_grant(key)
+    if not renew_grant(cluster_grant, scaled_time) then
+        return {}
     end
+
+    local quota_used = 0
+    for i = 0, SLICES - 1 do
+        quota_used = quota_used + cluster_grant[i]
+    end
+
     -- detect granted resource amount
-    local grant = math.min(get_max_lease(key), quota - quota_usage)
-    grant = math.max(grant, 0)
-    -- update info
-    history[1 + math.mod(stime, slices)] =
-        history[1 + math.mod(stime, slices)] + grant
-    -- if something was granted add lease info to gossip queue
-    if grant > 0 then
-        gossip_queue:put({
-            mode = 'push',
-            srv = box.info.server.uuid,
-            key = key,
-            stime = stime,
-            grant = grant})
+    local lease = mmax(mmin(max_lease, quota - quota_used), 0)
+    if lease == 0 then
+        return {}
     end
-    return {lease = grant, exp = (stime + 1) / slices}
+
+    local index = mmod(scaled_time, SLICES)
+    cluster_grant[index] = cluster_grant[index] + lease
+
+    local server_grant = get_server_grant(server_id, key)
+    renew_grant(server_grant, scaled_time)
+    server_grant[index] = server_grant[index] + lease
+    if lease > 0 then
+        gossip_queue:put({
+            server = server_id,
+            key = key,
+            scaled_time = scaled_time,
+            grant = server_grant[index]})
+    end
+    return {value = lease, expiration = time + 1 / SLICES}
 end
 
--- check if we have available reminders, or request lease if not
+-- check if we have available remainder, or request lease if not
 local function get_lease(user, domain, op, quota)
-    local key = get_key(user, domain, op)
-    local reminder = reminders[key]
+    local key = build_resource_key(user, domain, op)
     local time = fiber.time()
-    -- reminder is expired or have no availbale resource to grant
-    if reminder == nil or reminder.exp <= time or reminder.lease == 0 then
-        reminder = request_lease(key, quota, time)
-        reminders[key] = reminder
+    local remainder = remainders[key]
+    if not (remainder and remainder.expiration and remainder.expiration > time
+        and remainder.value and remainder.value > 0) then
+        remainder = request_lease(key, quota, time)
+        remainders[key] = remainder
     end
-    if reminder.lease > 0 then
-        reminder.lease = reminder.lease - 1
+    if remainder.value and remainder.value > 0 then
+        remainder.value = remainder.value - 1
         return true
     end
     return false
 end
 
-local function init_lease(p_conn_pool, p_neighbor, p_max_lease)
+local function init_leases(p_conn_pool, p_neighbour_count, p_max_lease)
     conn_pool = p_conn_pool
-    neighbor = p_neighbor
+    neighbour_count = p_neighbour_count
     max_lease = p_max_lease
-    gossip_queue = fiber.channel(1000)
+    server_id = box.info.server.uuid
+    gossip_queue = fiber.channel(100)
     fiber.create(gossip_fiber, gossip_queue)
 end
 
 
 return {
-    init = init_lease,
+    init = init_leases,
     get_lease = get_lease,
 }
